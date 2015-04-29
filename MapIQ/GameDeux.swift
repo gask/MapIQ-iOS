fe0e5420de7698e12d3c7e058dab4cb8a1098833
Giovanni <gbferraro@gmail.com>
2015-04-29 10:14:05 -0300

    Little adv

--- MapIQ/GameDeux.swift
+++ MapIQ/GameDeux.swift
@@ -10,11 +10,29 @@ import Foundation
 
 class GameDeux : UIViewController {
     
+    var mapWidth = Double(320)
+    var mapHeight = Double(320)
+    var mapName = "world"
+    
     @IBOutlet var map: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
+        
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        let screenSize = UIScreen.mainScreen().bounds
+        
+        if mapName == "world" {
+            println("oi")
+            map.frame = CGRect(x: Double(screenSize.width)/2-mapWidth/2, y: Double(screenSize.height)/2-mapHeight/2, width: Double(screenSize.width), height: Double(screenSize.width))
+            
+        }
     }
     
     @IBAction func tappedMap(sender: UITapGestureRecognizer) {
@@ -22,8 +40,6 @@ class GameDeux : UIViewController {
 
         var point = sender.locationInView(map)
         println("pt: \(point)")
-        
-        var mapWidth = Double(320)
         
         
         var lat = Double(-23.5479106)
@@ -56,5 +72,30 @@ class GameDeux : UIViewController {
         
         var latLon = CGPoint(x: pointLat, y: pointLng)
         println("latLon: \(latLon)")
+    }
+    
+    func getPixelPoint(coordinate: CGPoint) -> CGPoint {
+        
+        let x = (Double(coordinate.x) + 180.0) * (mapWidth/360.0)
+        
+        let latRad = Double(coordinate.y)*M_PI/180.0
+        let mercN = log(tan((M_PI/4)+(latRad/2)))
+        let y = (mapHeight/2)-(mapWidth*mercN/(2*M_PI))
+        
+        let pt = CGPoint(x: x, y: y)
+        
+        return pt
+    }
+    
+    func getCoordinate(pixelPoint: CGPoint) -> CGPoint {
+        let pointLng = Double(pixelPoint.x) / (mapWidth/360.0) - 180.0
+        
+        let antiMercN = (2.0*M_PI)/mapWidth * (mapHeight/2.0 - Double(pixelPoint.y))
+        let antiLatRad = 2*(atan(pow(M_E, antiMercN)) - M_PI/4)
+        let pointLat = 180/M_PI * antiLatRad
+        
+        let latLon = CGPoint(x: pointLat, y: pointLng)
+        
+        return latLon
     }
 }
